d5193f1c4348b2159f894c172035a95ac96e0024
thanhdang1709 <typhoom1709@gmail.com>
2022-05-06 17:09:33 +0700

    update

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -106,7 +106,8 @@ class AuthController extends Controller
                                 $check_code = User::where('code', $ref_code)->first();
                                 if($check_code) {
                                     User::where('id', $user->id)->update(['ref_code' => $ref_code]);
-                                    \DB::table('earns')->insert(['user_id' => $check_code->id, 'status' => 2, 'reward' => 1, 'subject' => 'ref', 'description' => 'Reward for ref', 'created_at' => Carbon::now()]);
+                                    \DB::table('earns')->insert(['user_id' => $check_code->id, 'status' => 2, 'reward' => 1, 'subject' => 'ref', 'description' => 'Reward from referral', 'created_at' => Carbon::now()]);
+                                    User::where('id', $user_id)->increment('balance',  1);
                                 }
                                 //  else {
                                 //     return $this->responseError('Invalid referral code', 201);
