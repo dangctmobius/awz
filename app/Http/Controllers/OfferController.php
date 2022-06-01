b8bd910eac77978b0442781f66ae0cea162a7117
thanhdang1709 <typhoom1709@gmail.com>
2022-06-01 17:16:11 +0700

    update

--- app/Http/Controllers/OfferController.php
+++ app/Http/Controllers/OfferController.php
@@ -9,6 +9,11 @@ class OfferController extends Controller
 {   
 
     public function offer_tapjoy(Request $request){
+        
+        $req_dump = print_r($request->all(), TRUE);
+        $fp = fopen('request.log', 'a');
+        fwrite($fp, $req_dump);
+        fclose($fp);
 
         return $this->responseOk($request->all());
 
