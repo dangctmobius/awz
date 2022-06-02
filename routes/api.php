5bb856d2318b50cc1427e73a397853e7c8806643
thanhdang1709 <typhoom1709@gmail.com>
2022-06-02 12:21:40 +0700

    update

--- routes/api.php
+++ routes/api.php
@@ -97,130 +97,130 @@ Route::group([
 
 
 
-Route::group([
-    'middleware' => 'api',
-    'prefix' => 'post'
+// Route::group([
+//     'middleware' => 'api',
+//     'prefix' => 'post'
 
-], function ($router) {
-    Route::get('/list', [PostController::class, 'index'])->middleware(['check_token','auth:api']);
-    Route::get('/list_by_tag', [PostController::class, 'list_by_tag'])->middleware(['check_token','auth:api']);
-    Route::post('/add', [PostController::class, 'store'])->middleware(['check_token','auth:api']);
-    Route::post('/like/{id}', [PostController::class, 'like'])->middleware(['check_token','auth:api']);
-    Route::post('/unlike/{id}', [PostController::class, 'unlike'])->middleware(['check_token','auth:api']);
-    Route::post('/delete/{id}', [PostController::class, 'destroy'])->middleware(['check_token','auth:api']);
-
-
-    Route::get('/list/by_user/{id}', [PostController::class, 'get_list_by_user'])->middleware(['check_token','auth:api']);
-});
+// ], function ($router) {
+//     Route::get('/list', [PostController::class, 'index'])->middleware(['check_token','auth:api']);
+//     Route::get('/list_by_tag', [PostController::class, 'list_by_tag'])->middleware(['check_token','auth:api']);
+//     Route::post('/add', [PostController::class, 'store'])->middleware(['check_token','auth:api']);
+//     Route::post('/like/{id}', [PostController::class, 'like'])->middleware(['check_token','auth:api']);
+//     Route::post('/unlike/{id}', [PostController::class, 'unlike'])->middleware(['check_token','auth:api']);
+//     Route::post('/delete/{id}', [PostController::class, 'destroy'])->middleware(['check_token','auth:api']);
 
 
-Route::group([
-    'middleware' => 'api',
-    'prefix' => 'tag'
-
-], function ($router) {
-    Route::get('/list', [TagController::class, 'list'])->middleware(['check_token','auth:api']);
-});
-
-Route::group([
-    'middleware' => 'api',
-    'prefix' => 'comment'
-
-], function ($router) {
-    Route::get('/list/{post_id}', [CommentController::class, 'get_comment_by_post_id'])->middleware(['check_token','auth:api']);
-    Route::post('/add', [CommentController::class, 'store'])->middleware(['check_token','auth:api']);
-});
+//     Route::get('/list/by_user/{id}', [PostController::class, 'get_list_by_user'])->middleware(['check_token','auth:api']);
+// });
 
 
+// Route::group([
+//     'middleware' => 'api',
+//     'prefix' => 'tag'
 
+// ], function ($router) {
+//     Route::get('/list', [TagController::class, 'list'])->middleware(['check_token','auth:api']);
+// });
 
-Route::group([
-    'middleware' => 'api',
-    'prefix' => 'earn'
+// Route::group([
+//     'middleware' => 'api',
+//     'prefix' => 'comment'
 
-], function ($router) {
-    Route::get('/list', [EarnController::class, 'list'])->middleware(['check_token','auth:api']);
-    Route::get('/list_today', [EarnController::class, 'list_today'])->middleware(['check_token','auth:api']);
-    Route::get('/earn', [EarnController::class, 'earn'])->middleware(['check_token','auth:api']);
-    Route::get('/earn_total', [EarnController::class, 'earn_total'])->middleware(['check_token','auth:api']);
-    Route::get('/list_chart', [EarnController::class, 'list_chart'])->middleware(['check_token','auth:api']);
-    Route::post('/earn_dice', [EarnController::class, 'earn_dice'])->middleware(['check_token','auth:api']);
-});
-
-
-Route::group([
-    'middleware' => 'api',
-    'prefix' => 'task'
-
-], function ($router) {
-    Route::get('/list', [TaskController::class, 'list'])->middleware(['check_token','auth:api']);
-    Route::post('/earn', [TaskController::class, 'earn'])->middleware(['check_token','auth:api']);
-});
-
-
-Route::group([
-    'middleware' => 'api',
-    'prefix' => 'ads'
-
-], function ($router) {
-    Route::get('/limit', [AdsController::class, 'limit'])->middleware(['check_token','auth:api']);
-    Route::post('/earn', [AdsController::class, 'earn'])->middleware(['check_token','auth:api']);
-    Route::get('/check_show_ads', [AdsController::class, 'check_show_ads'])->middleware(['check_token','auth:api']);
-});
+// ], function ($router) {
+//     Route::get('/list/{post_id}', [CommentController::class, 'get_comment_by_post_id'])->middleware(['check_token','auth:api']);
+//     Route::post('/add', [CommentController::class, 'store'])->middleware(['check_token','auth:api']);
+// });
 
 
 
-Route::group([
-    'middleware' => 'api',
-    'prefix' => 'kyc'
 
-], function ($router) {
-    Route::post('/add', [KycController::class, 'store'])->middleware(['check_token','auth:api']);
-});
+// Route::group([
+//     'middleware' => 'api',
+//     'prefix' => 'earn'
 
-
-Route::group([
-    'middleware' => 'api',
-    'prefix' => 'cashback'
-
-], function ($router) {
-    Route::get('/list_banner/{type}', [CashbackController::class, 'list_banner'])->middleware(['check_token','auth:api']);
-});
-
-Route::group([
-    'middleware' => 'api',
-    'prefix' => 'brand'
-
-], function ($router) {
-    Route::get('/list/{type}', [BrandController::class, 'list'])->middleware(['check_token','auth:api']);
-});
+// ], function ($router) {
+//     Route::get('/list', [EarnController::class, 'list'])->middleware(['check_token','auth:api']);
+//     Route::get('/list_today', [EarnController::class, 'list_today'])->middleware(['check_token','auth:api']);
+//     Route::get('/earn', [EarnController::class, 'earn'])->middleware(['check_token','auth:api']);
+//     Route::get('/earn_total', [EarnController::class, 'earn_total'])->middleware(['check_token','auth:api']);
+//     Route::get('/list_chart', [EarnController::class, 'list_chart'])->middleware(['check_token','auth:api']);
+//     Route::post('/earn_dice', [EarnController::class, 'earn_dice'])->middleware(['check_token','auth:api']);
+// });
 
 
-Route::group([
-    'middleware' => 'api',
-    'prefix' => 'airdrop'
+// Route::group([
+//     'middleware' => 'api',
+//     'prefix' => 'task'
 
-], function ($router) {
-    Route::get('/list', [AirdropController::class, 'list'])->middleware(['check_token','auth:api']);
-    Route::post('/submit', [AirdropSubmitController::class, 'submit'])->middleware(['check_token','auth:api']);
-});
-
-
-Route::group([
-    'middleware' => 'api',
-    'prefix' => 'product'
-
-], function ($router) {
-    Route::post('/order', [OrderController::class, 'store'])->middleware(['check_token','auth:api']);
-    Route::get('/list', [ProductController::class, 'list'])->middleware(['check_token','auth:api']);
-    Route::get('/detail/{id}', [ProductController::class, 'detail'])->middleware(['check_token','auth:api']);
-});
+// ], function ($router) {
+//     Route::get('/list', [TaskController::class, 'list'])->middleware(['check_token','auth:api']);
+//     Route::post('/earn', [TaskController::class, 'earn'])->middleware(['check_token','auth:api']);
+// });
 
 
-Route::group([
-    'middleware' => 'api',
-    'prefix' => 'order'
+// Route::group([
+//     'middleware' => 'api',
+//     'prefix' => 'ads'
 
-], function ($router) {
-    Route::get('/list', [OrderController::class, 'index'])->middleware(['check_token','auth:api']);
-});
+// ], function ($router) {
+//     Route::get('/limit', [AdsController::class, 'limit'])->middleware(['check_token','auth:api']);
+//     Route::post('/earn', [AdsController::class, 'earn'])->middleware(['check_token','auth:api']);
+//     Route::get('/check_show_ads', [AdsController::class, 'check_show_ads'])->middleware(['check_token','auth:api']);
+// });
+
+
+
+// Route::group([
+//     'middleware' => 'api',
+//     'prefix' => 'kyc'
+
+// ], function ($router) {
+//     Route::post('/add', [KycController::class, 'store'])->middleware(['check_token','auth:api']);
+// });
+
+
+// Route::group([
+//     'middleware' => 'api',
+//     'prefix' => 'cashback'
+
+// ], function ($router) {
+//     Route::get('/list_banner/{type}', [CashbackController::class, 'list_banner'])->middleware(['check_token','auth:api']);
+// });
+
+// Route::group([
+//     'middleware' => 'api',
+//     'prefix' => 'brand'
+
+// ], function ($router) {
+//     Route::get('/list/{type}', [BrandController::class, 'list'])->middleware(['check_token','auth:api']);
+// });
+
+
+// Route::group([
+//     'middleware' => 'api',
+//     'prefix' => 'airdrop'
+
+// ], function ($router) {
+//     Route::get('/list', [AirdropController::class, 'list'])->middleware(['check_token','auth:api']);
+//     Route::post('/submit', [AirdropSubmitController::class, 'submit'])->middleware(['check_token','auth:api']);
+// });
+
+
+// Route::group([
+//     'middleware' => 'api',
+//     'prefix' => 'product'
+
+// ], function ($router) {
+//     Route::post('/order', [OrderController::class, 'store'])->middleware(['check_token','auth:api']);
+//     Route::get('/list', [ProductController::class, 'list'])->middleware(['check_token','auth:api']);
+//     Route::get('/detail/{id}', [ProductController::class, 'detail'])->middleware(['check_token','auth:api']);
+// });
+
+
+// Route::group([
+//     'middleware' => 'api',
+//     'prefix' => 'order'
+
+// ], function ($router) {
+//     Route::get('/list', [OrderController::class, 'index'])->middleware(['check_token','auth:api']);
+// });
