7b12580b1e07bf47bf8486de3cb5e01f509e3ca6
thanhdang1709 <typhoom1709@gmail.com>
2022-06-02 10:58:40 +0700

    update

--- routes/web.php
+++ routes/web.php
@@ -30,10 +30,10 @@ Route::get('login', function () {
 
 
 Route::get('testnet', function () {
-    $users = User::select('email', 'address', 'balance')->orderBy('balance', 'desc')->get();
-    echo 'email,address,balance<br>';
+    $users = User::select('address', 'balance')->whereNotNull('address')->where('address', '<>', '')->orderBy('balance', 'desc')->get();
+    echo 'Wallet,Point<br>';
     foreach($users as $user) {
-        echo $user->email . ',' . ($user->address ?? 'ADDRESS_NULL')    . ',' . $user->balance . '<br>';
+        echo $user->address. ',' . $user->balance . '<br>';
     }
 })->name('testnet');
 
