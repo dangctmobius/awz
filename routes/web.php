404161b86678568f9f5c9df07fbb3723c3e5398a
thanhdang1709 <typhoom1709@gmail.com>
2022-06-03 22:07:24 +0700

    update

--- routes/web.php
+++ routes/web.php
@@ -28,6 +28,9 @@ Route::get('login', function () {
 //     return view(('mail_send'));
 // });
 
+Route::get('/tracking', function () {
+    return  redirect('https://amazon.com');;
+});
 
 Route::get('testnet', function () {
     $users = User::select('address', 'balance')->whereNotNull('address')->where('address', '<>', '')->orderBy('balance', 'desc')->get();
