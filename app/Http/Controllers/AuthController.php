71ad2773bc9d5786b680bb08db013dd9948eaff4
thanhdang1709 <typhoom1709@gmail.com>
2022-06-05 19:59:21 +0700

    update

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -108,14 +108,14 @@ class AuthController extends Controller
                                 if($check_code) {
                                     User::where('id', $user->id)->update(['ref_code' => $ref_code]);
                                     
-                                    $price = $this->getPrice();
-                                    $reward =  (double)env('POINT_REWARD_REF') / $price;
+                                    // $price = $this->getPrice();
+                                    // $reward =  (double)env('POINT_REWARD_REF') / $price;
 
-                                    $total_earn = Earn::where('user_id', $user->id)->where('subject', 'ref')->whereDate('created_at', Carbon::today())->count();
-                                    if($total_earn < (int)env('LIMIT_ADS_VIDEO')) {
-                                        \DB::table('earns')->insert(['user_id' => $check_code->id, 'status' => 1, 'reward' => $reward, 'subject' => 'ref', 'description' => 'Reward from referral', 'created_at' => Carbon::now()]);
-                                        User::where('id', $check_code->id)->increment('pending_balance',   $reward);
-                                    }
+                                    // $total_earn = Earn::where('user_id', $user->id)->where('subject', 'ref')->whereDate('created_at', Carbon::today())->count();
+                                    // if($total_earn < (int)env('LIMIT_ADS_VIDEO')) {
+                                        \DB::table('earns')->insert(['user_id' => $check_code->id, 'status' => 1, 'reward' => 7, 'subject' => 'ref', 'description' => 'Reward from referral', 'created_at' => Carbon::now()]);
+                                        User::where('id', $check_code->id)->increment('pending_balance', 7);
+                                    // }
                                 }
                                 //  else {
                                 //     return $this->responseError('Invalid referral code', 201);
