54a9ed9bd24162019ad17eef87c8df875a733744
thanhdang1709 <typhoom1709@gmail.com>
2022-06-12 15:46:51 +0700

    Update AuthController.php

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -111,11 +111,15 @@ class AuthController extends Controller
                                     $price = $this->getPrice();
                                     $reward =  (double)env('POINT_REWARD_REF') / $price;
 
-                                    $total_earn = Earn::where('user_id', $user->id)->where('subject', 'ref')->whereDate('created_at', Carbon::today())->count();
-                                    if($total_earn < (int)env('LIMIT_ADS_VIDEO')) {
-                                        \DB::table('earns')->insert(['user_id' => $check_code->id, 'status' => 1, 'reward' => intval($reward), 'subject' => 'ref', 'description' => 'Reward from referral', 'created_at' => Carbon::now()]);
-                                        User::where('id', $check_code->id)->increment('pending_balance',  intval($reward));
+                                    
+                                    if($owner_user){
+                                        $total_earn = Earn::where('user_id', $check_code->id)->where('subject', 'ref')->whereDate('created_at', Carbon::today())->count();
+                                        if($total_earn < (int)env('LIMIT_ADS_VIDEO')) {
+                                            \DB::table('earns')->insert(['user_id' => $check_code->id, 'status' => 1, 'reward' => intval($reward), 'subject' => 'ref', 'description' => 'Reward from referral', 'created_at' => Carbon::now()]);
+                                            User::where('id', $check_code->id)->increment('pending_balance',  intval($reward));
+                                        }
                                     }
+                                    
                                 }
                                  else {
                                     return $this->responseError('Invalid referral code', 201);
