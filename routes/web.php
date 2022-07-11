240a76c8d5e8e4c9ba834f94ecee4d46dd26d683
thanhdang1709 <typhoom1709@gmail.com>
2022-07-11 23:30:36 +0700

    update

--- routes/web.php
+++ routes/web.php
@@ -62,12 +62,13 @@ Route::get('/payments/offers/tapjoy', [OfferController::class, 'offer_tapjoy']);
 
 Route::get('/send_fcm', function () {
 
+    $url = 'https://fcm.googleapis.com/fcm/send';
+
     $response = Http::withHeaders([
         'Authorization' => env('FCM_ADMIN_TOKEN'),
         'Content-Type' => 'application/json' 
-    ])->post('https://fcm.googleapis.com/fcm/send', [
-        // 'to' => '/topics/all',
-        'registration_ids' => ['e_Y_zNd3T-CWLnNomDQw7F:APA91bGUelYw2JcK3d-IbXjiBBgQ8ycCOtJ34eU-x4w54ZfbufhzWmFOYGHCkIQxHjqT0D8k9aWPFGSdYHO5rWoMrTPy9o70GNL91Gpot9oIuk1t-27G5DSNI3txpl-O1tkf8idUNtK1', 'cezk2jqgQzS2DqzVIeAsmq:APA91bGU6tQswep9t_oJ0V0b6_hPpWzEF7ZA_F5S0wkTwsfUv0PQmHywsnD6vDoKeE2SXSoY9l5rlMN_QjW92pfjjqLyxD7QHiZneHkhwZKmDhhANXkL9QBb18lenYt0zt4vo0VGItmR', 'cl3Tj9BSm0PPjUrY9HEvFN:APA91bEvRAlTYwWQ_1XL_uTCJFGkKE9qdnnarHNICMAUm2O0jQNENYjmynhFEL4gFqXu-235AT0FnoY8bD0axC7V_ku-mXQhBblFVbuVcArxtK2sZBU3-sOFd-pIj0yhmXrSVvWvE6Ri'],
+    ])->post($url, [
+        'registration_ids' => ['id'],
         'notification'=>[
             "title"=> "AZ World SocialFi",
             "body"=> "Refund your purchase. Reason: Must have old account of Spotify Before",
